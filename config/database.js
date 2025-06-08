87e9ab103e4155e09db4a239550873bb64ec2e9f
manpreet8021 <54615943+manpreet8021@users.noreply.github.com>
2025-06-09 01:01:39 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ import UserImage from "../model/userImagesModel.js";
 import Event from "../model/eventModel.js";
 import UserLocation from "../model/userLocationModel.js";
 import EventDate from "../model/eventDateModel.js";
-import UserLookingFor from "../model/UserLookingForModel.js";
+import UserLookingFor from "../model/userLookingForModel.js";
 
 const connectDb = async () => {
   await sequelize
